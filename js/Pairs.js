c4660e16c9298937aa7538e2d0215375a6f45bc7
anassbt <anass.boumssimrat@gmail.com>
2017-07-25 11:08:42 +0200

    Creating Js of the Memory Game

--- js/Pairs.js
+++ js/Pairs.js
@@ -1,5 +1,44 @@
 // Memory game //
 
+main {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 3%;
+  align-items: center;
+  padding-left: 10%;
+}
+
+img:hover {
+  background-color: red;
+}
+
+div  {
+  background-image: url(img/backcard.jpg);
+  opacity: 1;
+  justify-content: space-around;
+  width: 20%;
+  height: 150px;
+  margin: 5px;
+}
+img {
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+}
+h1 , p {
+  text-align: center;
+}
+
+span {
+  color: red;
+}
+
+main {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+}
 
 
 // Memory game //
